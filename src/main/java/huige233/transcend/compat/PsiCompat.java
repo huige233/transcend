4eae0c94c42b3deb7324b508fb6187670528ceca
WarmthDawn <warmth06@sina.com>
2022-04-16 15:28:22 +0800

    :sparkles: psi

---
+++ src/main/java/huige233/transcend/compat/PsiCompat.java
@@ -0,0 +1,46 @@
+package huige233.transcend.compat;
+
+import huige233.transcend.util.ArmorUtils;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import vazkii.psi.api.PsiAPI;
+import vazkii.psi.api.cad.ICAD;
+import vazkii.psi.api.cad.RegenPsiEvent;
+import vazkii.psi.common.core.handler.PlayerDataHandler;
+
+@Mod.EventBusSubscriber
+public class PsiCompat {
+    public static boolean enabled = false;
+
+    public static void onPlayerAttack(EntityPlayer player, EntityPlayer attacker) {
+        if (!enabled) return;
+        if(player.world.isRemote) return;
+        PlayerDataHandler.PlayerData data = PlayerDataHandler.get(attacker);
+        ItemStack cadItem = PsiAPI.getPlayerCAD(attacker);
+        if(cadItem != null && cadItem.getItem() instanceof ICAD) {
+            ICAD cad = (ICAD) cadItem.getItem();
+            int storedPsi = cad.getStoredPsi(cadItem);
+//            data.deductPsi(data.getAvailablePsi() + storedPsi, cd, true);
+        }else {
+//            data.deductPsi(data.getAvailablePsi(), cd, true);
+        }
+
+    }
+
+
+
+
+    @SubscribeEvent
+    public static void onPsiRegen(RegenPsiEvent event) {
+        if (!enabled) return;
+        EntityPlayer player = event.getPlayer();
+        if(player.world.isRemote) return;
+        if(ArmorUtils.fullEquipped(player)){
+//            event.addRegen(5000);
+        }
+    }
+
+
+}
--- src/main/java/huige233/transcend/items/armor/ArmorBase.java
+++ src/main/java/huige233/transcend/items/armor/ArmorBase.java
@@ -1,10 +1,13 @@
 package huige233.transcend.items.armor;
 
 import huige233.transcend.Main;
+import huige233.transcend.compat.PsiCompat;
 import huige233.transcend.init.ModItems;
+import huige233.transcend.util.ArmorUtils;
 import huige233.transcend.util.IHasModel;
 import huige233.transcend.util.Reference;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
@@ -23,115 +26,106 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.NotNull;
+import vazkii.psi.api.PsiAPI;
 
-@Mod.EventBusSubscriber( modid = Reference.MOD_ID )
+@Mod.EventBusSubscriber(modid = Reference.MOD_ID)
 public class ArmorBase extends ItemArmor implements IHasModel {
-    public ArmorBase( String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn, CreativeTabs tab )
-    {
+    public ArmorBase(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn, CreativeTabs tab) {
         super(materialIn, renderIndexIn, equipmentSlotIn);
-        setTranslationKey( name );
-        setRegistryName( name );
-        setCreativeTab( tab );
-        ModItems.ITEMS.add( this );
+        setTranslationKey(name);
+        setRegistryName(name);
+        setCreativeTab(tab);
+        ModItems.ITEMS.add(this);
     }
 
 
     @Override
-    public void registerModels()
-    {
-        Main.proxy.registerItemRenderer( this, 0, "inventory" );
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(this, 0, "inventory");
     }
 
 
-    @SubscribeEvent( priority = EventPriority.HIGHEST )
-    public static void onPlayerDeath( LivingDeathEvent event )
-    {
-        if ( !(event.getEntityLiving() instanceof EntityPlayer) || event.isCanceled() )
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void onPlayerDeath(LivingDeathEvent event) {
+        if (!(event.getEntityLiving() instanceof EntityPlayer) || event.isCanceled())
             return;
         EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-        if ( !player.isServerWorld() )
+        if (!player.isServerWorld())
             return;
         NonNullList<ItemStack> armor = player.inventory.armorInventory;
-        if ( armor.get( 3 ).getItem() == ModItems.FLAWLESS_HELMET && armor.get( 3 ).getItem() == ModItems.FLAWLESS_CHESTPLATE && armor.get( 1 ).getItem() == ModItems.FLAWLESS_LEGGINGS && armor.get( 0 ).getItem() == ModItems.FLAWLESS_BOOTS )
-        {
-            if ( player.getHealth() <= 0 )
-            {
-                event.setCanceled( true );
+        if (armor.get(3).getItem() == ModItems.FLAWLESS_HELMET && armor.get(3).getItem() == ModItems.FLAWLESS_CHESTPLATE && armor.get(1).getItem() == ModItems.FLAWLESS_LEGGINGS && armor.get(0).getItem() == ModItems.FLAWLESS_BOOTS) {
+            if (player.getHealth() <= 0) {
+                event.setCanceled(true);
                 float maxHP = player.getMaxHealth();
-                event.getEntityLiving().setHealth( maxHP );
+                event.getEntityLiving().setHealth(maxHP);
             }
         }
     }
 
 
-    @SubscribeEvent( priority = EventPriority.HIGHEST )
-    public static void onPlayerHurt( LivingHurtEvent event )
-    {
-        if ( !(event.getEntityLiving() instanceof EntityPlayer) || event.isCanceled() )
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void onPlayerHurt(LivingHurtEvent event) {
+        if (!(event.getEntityLiving() instanceof EntityPlayer) || event.isCanceled())
             return;
         EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-        if ( !player.isServerWorld() )
+        if (!player.isServerWorld())
             return;
-        NonNullList<ItemStack> armor = player.inventory.armorInventory;
-        if ( armor.get( 3 ).getItem() == ModItems.FLAWLESS_HELMET && armor.get( 2 ).getItem() == ModItems.FLAWLESS_CHESTPLATE && armor.get( 1 ).getItem() == ModItems.FLAWLESS_LEGGINGS && armor.get( 0 ).getItem() == ModItems.FLAWLESS_BOOTS )
-        {
-            event.setCanceled( true );
+        if (ArmorUtils.fullEquipped(player)) {
+            event.setCanceled(true);
         }
     }
 
 
-    @SubscribeEvent( priority = EventPriority.HIGHEST )
-    public static void LivingAttackEvent( LivingAttackEvent event )
-    {
-        if ( !(event.getEntityLiving() instanceof EntityPlayer) || event.isCanceled() )
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void LivingAttackEvent(LivingAttackEvent event) {
+        if (!(event.getEntityLiving() instanceof EntityPlayer) || event.isCanceled())
             return;
         EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-        if ( player.world.isRemote )
+        if (player.world.isRemote)
             return;
         NonNullList<ItemStack> armor = player.inventory.armorInventory;
-        if ( armor.get( 3 ).getItem() == ModItems.FLAWLESS_HELMET && armor.get( 2 ).getItem() == ModItems.FLAWLESS_CHESTPLATE && armor.get( 1 ).getItem() == ModItems.FLAWLESS_LEGGINGS && armor.get( 0 ).getItem() == ModItems.FLAWLESS_BOOTS )
-        {
-            event.setCanceled( true );
+        if (armor.get(3).getItem() == ModItems.FLAWLESS_HELMET && armor.get(2).getItem() == ModItems.FLAWLESS_CHESTPLATE && armor.get(1).getItem() == ModItems.FLAWLESS_LEGGINGS && armor.get(0).getItem() == ModItems.FLAWLESS_BOOTS) {
+
+            Entity attacker = event.getSource().getTrueSource();
+            if (attacker instanceof EntityPlayer) {
+                PsiCompat.onPlayerAttack(player, (EntityPlayer) attacker);
+            }
+
+            event.setCanceled(true);
         }
+
     }
 
 
     @Override
-    public void onArmorTick(@NotNull World world, @NotNull EntityPlayer player, @NotNull ItemStack itemStack )
-    {
-        if ( this.armorType == EntityEquipmentSlot.HEAD )
-        {
-            player.setAir( 300 );
-            player.addPotionEffect( new PotionEffect( MobEffects.RESISTANCE, 300, 14, false, false ) );
-            player.addPotionEffect( new PotionEffect( MobEffects.NIGHT_VISION, 300, 0, false, false ) );
-        } else if ( this.armorType == EntityEquipmentSlot.CHEST )
-        {
+    public void onArmorTick(@NotNull World world, @NotNull EntityPlayer player, @NotNull ItemStack itemStack) {
+        if (this.armorType == EntityEquipmentSlot.HEAD) {
+            player.setAir(300);
+            player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 300, 14, false, false));
+            player.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 300, 0, false, false));
+        } else if (this.armorType == EntityEquipmentSlot.CHEST) {
             player.capabilities.allowFlying = true;
-            player.addPotionEffect( new PotionEffect( MobEffects.STRENGTH, 300, 14, false, false ) );
-            player.addPotionEffect( new PotionEffect( MobEffects.REGENERATION, 300, 14, false, false ) );
-        } else if ( this.armorType == EntityEquipmentSlot.LEGS )
-        {
-            player.getFoodStats().addStats( 20, 20.0f );
-            player.addPotionEffect( new PotionEffect( MobEffects.SPEED, 300, 2, false, false ) );
-            player.addPotionEffect( new PotionEffect( MobEffects.JUMP_BOOST, 300, 2, false, false ) );
-        } else if ( this.armorType == EntityEquipmentSlot.FEET )
-        {
+            player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 300, 14, false, false));
+            player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 300, 14, false, false));
+        } else if (this.armorType == EntityEquipmentSlot.LEGS) {
+            player.getFoodStats().addStats(20, 20.0f);
+            player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 300, 2, false, false));
+            player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 300, 2, false, false));
+        } else if (this.armorType == EntityEquipmentSlot.FEET) {
             player.setFire(0);
-            player.addPotionEffect( new PotionEffect( MobEffects.LUCK, 300, 9, false, false ) );
-            player.addPotionEffect( new PotionEffect( MobEffects.HASTE, 300, 44, false, false ) );
+            player.addPotionEffect(new PotionEffect(MobEffects.LUCK, 300, 9, false, false));
+            player.addPotionEffect(new PotionEffect(MobEffects.HASTE, 300, 44, false, false));
         }
     }
 
 
-    @SideOnly( Side.CLIENT )
-    public boolean hasEffect(@NotNull ItemStack stack )
-    {
-        return(false);
+    @SideOnly(Side.CLIENT)
+    public boolean hasEffect(@NotNull ItemStack stack) {
+        return (false);
     }
 
 
-    public @NotNull EnumRarity getRarity(@NotNull ItemStack stack )
-    {
-        return(ModItems.COSMIC_RARITY);
+    public @NotNull EnumRarity getRarity(@NotNull ItemStack stack) {
+        return (ModItems.COSMIC_RARITY);
     }
 }
